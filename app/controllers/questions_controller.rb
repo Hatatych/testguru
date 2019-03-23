19a45e30152ceef7f71303b4943cd9a09886f94b
Hatatych <me@hataty.ch>
2019-03-23 13:10:39 +0300

    Questions through associations

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,38 +2,36 @@ class QuestionsController < ApplicationController
   before_action :find_test
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  # как мне обрабатывать исключения ТОЛЬКО для вопросов?
-  # тк сейчас он ругается ответом про Question даже если не найден тест в роуте /tests/20
 
   def index
     render inline: 'Questions: <%= @test.questions.inspect %>'
   end
 
   def show
-    render inline: 'Question: <%= Question.find(request.params[:id]).inspect %> <br/><br/> Answers: <%= Question.find(request.params[:id]).answers.inspect %>'
+    render inline: 'Question: <%= Question.find(params[:id]).inspect %> <br/><br/> Answers: <%= Question.find(params[:id]).answers.inspect %>'
   end
 
   def new; end
 
   def create
-    question = Question.create!(question_params)
+    question = Test.find(params[:test_id]).questions.create!(question_params)
 
     render plain: question.inspect
   end
 
   def destroy
-    Question.find(request.params[:test_id]).destroy
+    Question.find(params[:test_id]).destroy
     render plain: 'Deleted'
   end
 
   private
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def find_test
-    @test = Test.find(request.params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
   def rescue_with_question_not_found
