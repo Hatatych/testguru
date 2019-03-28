00b8f2c9b4217dce07b02b58e1c1ad69dac50bef
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-03-28 18:03:17 +0300

    Added some spice and routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,14 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, except: :edit
+  before_action :find_test, only: [:index, :create, :new]
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
-  # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = @test.questions
   end
 
-  def show
-    @question = @test.questions.find(params[:id])
-  end
+  def show; end
 
   def new
     @question = Question.new
@@ -19,19 +18,27 @@ class QuestionsController < ApplicationController
     @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to @test
+      redirect_to test_questions_path
     else
       render :new
     end
   end
 
-  def edit
-    @question = Question.find(params[:id])
+  def edit; end
+
+  def update
+    @question.update(question_params)
+
+    if @question.save
+      redirect_to test_questions_path(@question.test.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    Question.find(params[:test_id]).destroy
-    render plain: 'Deleted'
+    @question.destroy
+    redirect_to test_questions_path(@question.test.id)
   end
 
   private
@@ -42,6 +49,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,9 @@
 module QuestionsHelper
   def question_header
-
+    case params[:action]
+    when 'new' then "<h1>Create new #{@test.title} question</h1>".html_safe
+    when 'edit' then "<h1>Edit #{@question.test.title} question</h1>".html_safe
+    else "<h1>Undefined action</h1>".html_safe
+    end
   end
 end
