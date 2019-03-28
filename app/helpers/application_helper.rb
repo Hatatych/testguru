850d4262fd631cb4f92163d710ad9a964a27ae32
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-03-28 18:08:57 +0300

    Lil app helper fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def current_year
-    Time.now.year
+    Time.current.year
   end
 
   def create_github_link(author_name, repo_name)
-    "<a target=\"_blank\" href=\"https://github.com/#{author_name}/#{repo_name}\">Автор: #{author_name}</a>".html_safe
+    link_to "Автор: #{author_name}", "https://github.com/#{author_name}/#{repo_name}"
   end
 end
