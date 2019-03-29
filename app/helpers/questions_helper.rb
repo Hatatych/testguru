4616bd941cde86dcad7f6ac802b61ed487c8ee01
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-03-29 11:13:04 +0300

    Question helper logic update

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,6 @@
 module QuestionsHelper
-  def question_header
-    case params[:action]
-    when 'new' then "<h1>Create new #{@test.title} question</h1>".html_safe
-    when 'edit' then "<h1>Edit #{@question.test.title} question</h1>".html_safe
-    else "<h1>Undefined action</h1>".html_safe
-    end
+  def question_header(question)
+    return "<h1>Create new #{@test.title} question</h1>".html_safe if question.new_record?
+    "<h1>Edit #{@question.test.title} question</h1>".html_safe
   end
 end
