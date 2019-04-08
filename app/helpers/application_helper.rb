c515de591a27ec8ab78f1d0786351e8e4cd6d9ab
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-04-08 16:33:28 +0300

    Styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def flash_alert(message)
     flash[:alert] = message
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then 'alert alert-primary'
+    when 'success' then 'alert alert-success'
+    when 'error' then 'alert alert-danger'
+    when 'alert' then 'alert alert-warning'
+    end
+  end
 end
