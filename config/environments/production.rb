2a4ef270a6bd52ea836decc8a6b7a9dd89052d6b
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-04-11 15:13:30 +0300

    Host fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'blooming-harbor-86508.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'hatatestguru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
