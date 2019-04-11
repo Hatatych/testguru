7b828f799cf561a315f7feeff7bfda16135b431e
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-04-11 13:02:52 +0300

    More mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'blooming-harbor-86508.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
